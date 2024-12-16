5f043137fa9c777e88012bb33280205332a34774
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-12-16 21:03:00 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -83,11 +83,13 @@ app.post('/users/login', async (req, res) => {
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.status(200).json({
+            token,
+            user: {
                 id: user._id,
                 username: user.username,
                 email: user.email,
-                profilePic: user.profilePic,
-                token: token
+                profilePic: user.profilePic
+            }
         });
     } catch (err) {
         res.status(400).json({ message: err.message });
